3d0c2c3f689ec1c614cb423f38ca5417ea83a610
semin@bamcard-windows <music.semin@gmail.com>
2017-09-06 16:06:58 +0200

    Add components

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,20 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <nav>
+      <div class="container">
+        <ul class="nav__left">
+          <li>
+            <router-link to="/">Home</router-link>
+          </li>
+          <li>
+            <router-link to="/admin">Admin</router-link>
+          </li>
+          <li>
+            <router-link to="/cart">Cart</router-link>
+          </li>
+        </ul>
+      </div>
+    </nav>
     <router-view></router-view>
   </div>
 </template>
@@ -10,14 +24,3 @@ export default {
   name: 'app',
 };
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Hello from '@/components/Hello';
+import Home from '@/pages/Home';
+import Admin from '@/pages/Admin';
+import Cart from '@/pages/Cart';
 
 Vue.use(Router);
 
@@ -8,8 +10,18 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Hello',
-      component: Hello,
+      name: 'Home',
+      component: Home,
+    },
+    {
+      path: '/admin',
+      name: 'Admin',
+      component: Admin,
+    },
+    {
+      path: '/cart',
+      name: 'Cart',
+      component: Cart,
     },
   ],
 });
